e9951bfb3a59d8bf5d1f36f01ae129b55d4053c2
GovarthananD <govarthananduraisamy@gmail.com>
2025-10-20 23:28:31 +0530

    login update

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import { Toaster, toast } from "sonner";
 import API from "../services/api";
 
